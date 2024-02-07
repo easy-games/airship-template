3a700d1a2cc6b99d85d6dd477cc93711650663f1
Luke Chatton <chattonluke@gmail.com>
2024-02-06 17:25:56 -0800

    fix ts

--- Assets/Typescript~/src/Shared/GameManager.ts
+++ Assets/Typescript~/src/Shared/GameManager.ts
@@ -1,5 +1,5 @@
-import { Airship } from "@@Easy/Core/Shared/Airship";
-import { RunUtil } from "@@Easy/Core/Shared/Util/RunUtil";
+import { Airship } from "@Easy/Core/Shared/Airship";
+import { RunUtil } from "@Easy/Core/Shared/Util/RunUtil";
 
 export default class GameManager extends AirshipBehaviour {
 	override Start(): void {
