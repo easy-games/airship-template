abaf7ef3381b459d81c5fc8e8f7eb8de9db7546c
Jonathan Holmes <Vorlias@users.noreply.github.com>
2024-03-11 16:01:23 +1300

    ItemType fix for template

--- Assets/Typescript~/src/Shared/Inventory/InventoryItems.ts
+++ Assets/Typescript~/src/Shared/Inventory/InventoryItems.ts
@@ -1,11 +1,7 @@
-import { ItemType } from "@Easy/Core/Shared/Item/ItemType";
 import { ItemRegistrationConfig, ItemUtil } from "@Easy/Core/Shared/Item/ItemUtil";
 
-// Create custom ItemTypes
-declare module "@Easy/Core/Shared/Item/ItemType" {
-	export const enum ItemType {
-		EXAMPLE_ITEM = "EXAMPLE_ITEM",
-	}
+export const enum ItemType {
+	EXAMPLE_ITEM = "EXAMPLE_ITEM",
 }
 
 // Add custom fields to ItemMeta
