bfb370f0e7924ac1492d980fd373db98095cd071
christopher-buss <christopher.buss+github@protonmail.com>
2025-07-23 23:43:18 +0100

    Remove un-used Prettier plugins, enforce Prettier config

--- Assets/Typescript~/eslint.config.ts
+++ Assets/Typescript~/eslint.config.ts
@@ -1,13 +1,16 @@
 import tseslint from "typescript-eslint";
 import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
-	languageOptions: {
-		parserOptions: {
-			projectService: {
-				allowDefaultProject: ["*.ts"],
-				defaultProject: "../tsconfig.json",
+export default tseslint
+	.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
+		languageOptions: {
+			parserOptions: {
+				projectService: {
+					allowDefaultProject: ["*.ts"],
+					defaultProject: "../tsconfig.json",
+				},
 			},
 		},
-	},
-});
+	})
+	.push(eslintConfigPrettier);
