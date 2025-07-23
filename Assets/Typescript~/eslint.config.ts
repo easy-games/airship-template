00192a3c0f057a258ee4053f8c03648f62da2dee
christopher-buss <christopher.buss+github@protonmail.com>
2025-07-23 23:38:04 +0100

    Make ESLint not complain for *.ts config file

--- Assets/Typescript~/eslint.config.ts
+++ Assets/Typescript~/eslint.config.ts
@@ -2,10 +2,12 @@ import tseslint from "typescript-eslint";
 import eslint from "@eslint/js";
 
 export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
-	files: ["Code/**/*.ts"],
 	languageOptions: {
 		parserOptions: {
-			projectService: true,
+			projectService: {
+				allowDefaultProject: ["*.ts"],
+				defaultProject: "../tsconfig.json",
+			},
 		},
 	},
 });
