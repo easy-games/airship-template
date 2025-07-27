c252071fe72f6eb0e222f35214a3d0cf40de20ce
Jonathan Holmes <Vorlias@users.noreply.github.com>
2025-07-27 17:27:18 +1200

    Add some more sane ESLint defaults
    Adding more sane defaults for certain rules

--- Assets/Typescript~/eslint.config.ts
+++ Assets/Typescript~/eslint.config.ts
@@ -16,4 +16,12 @@ export default tseslint.config(
 		},
 	},
 	eslintConfigPrettier,
+	{
+		rules: {
+			"prefer-const": "warn", // prefer warning
+			"@typescript-eslint/no-unused-vars": "warn", // prefer warning
+			"@typescript-eslint/ban-ts-comment": "error", // AirshipTS does not allow this
+			"@typescript-eslint/no-namespace": "off", // Namespace syntax is allowed
+		},
+	},
 );
